12397f1c06573160ae03ad5ff23592a542b5338b
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-23 19:38:36 -0600

    basic react configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("empezamos");
+import React from "react";
+import ReactDOM from "react-dom";
+
+// ReactDOM.render(que vamos a renderizar, donde lo vamos a renderizar)
+ReactDOM.render(
+  <h1>Hello world seguimos avanzando con react</h1>,
+  document.getElementById("app")
+);
