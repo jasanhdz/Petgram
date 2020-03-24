f79c4adce1a680a6f1e7502f9bedac30a5b12645
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-23 23:05:21 -0600

    creating listOfCategories and global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 
 // ReactDOM.render(que vamos a renderizar, donde lo vamos a renderizar)
 ReactDOM.render(
-  <h1>Hello world seguimos avanzando con react</h1>,
+  <App />,
   document.getElementById('app')
 )
