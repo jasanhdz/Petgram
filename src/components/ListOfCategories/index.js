f9ba2e8399c9422091844fb09c5555bf683ef494
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-24 14:30:00 -0600

    using useState in ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgramserver-gp0xvypt8.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        return setCategories(response)
+      })
+    return () => {
+      console.log('ComponentWillUnMounts')
+    }
+  }, [])
   return (
     <List>
       {
