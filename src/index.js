c72194f50117226d96bae2317c45d284e4c78d12
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-23 21:03:36 -0600

    configuration the linter standard and deploy in now.sh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 // ReactDOM.render(que vamos a renderizar, donde lo vamos a renderizar)
 ReactDOM.render(
   <h1>Hello world seguimos avanzando con react</h1>,
-  document.getElementById("app")
-);
+  document.getElementById('app')
+)
