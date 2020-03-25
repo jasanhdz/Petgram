92c3e8c0180af32e7b649f23cd0220c80470bb88
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-24 23:51:13 -0600

    using render props to protect our routes

---
+++ src/pages/Favs.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Favs = () => (
+  <h1>This is Favs</h1>
+)
---
+++ src/pages/NotRegisterUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotRegisterUser = () => (
+  <h1>This is NotRegisterUser</h1>
+)
