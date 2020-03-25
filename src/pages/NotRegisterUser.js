588ca47b1a75ebd2eeeef8cac9cb70aaf8832b20
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-25 16:53:19 -0600

    exporting the context to use the authenticateAuth in a simplified way without the need to wrap them to the children

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -1,68 +1,70 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { RegisterMutation } from '../containers/RegisterMutation'
 import { Register } from '../components/Register'
 import { LoginMutation } from '../containers/LoginMutation'
 import { Login } from '../components/Login'
 
-export const NotRegisterUser = () => (
-  <Context.Consumer>
-    {/* {registerMutation} */}
-    {loginMutation}
-  </Context.Consumer>
-)
+export const NotRegisterUser = () => {
+  const { activateAuth } = useContext(Context)
 
-const loginMutation = ({ isAuth, activateAuth }) => {
-  return (
-    <LoginMutation>
-      {
-        (login, { data, loading, error }) => {
-          const onSubmit = ({ email, password }) => {
-            const input = { email, password }
-            const variables = { input }
-            login({ variables })
-              .then(({ data }) => {
-                const { login } = data
-                console.log(login)
-                activateAuth(login)
-              })
+  const loginMutation = () => {
+    return (
+      <LoginMutation>
+        {
+          (login, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              login({ variables })
+                .then(({ data }) => {
+                  const { login } = data
+                  console.log(login)
+                  activateAuth(login)
+                })
+            }
+            const errorMsg = error && 'Error al iniciar sesión alguno de los campos es incorrecto'
+            return (
+              <Login
+                disabled={loading}
+                onSubmit={onSubmit}
+                error={errorMsg}
+              />
+            )
           }
-          const errorMsg = error && 'Error al iniciar sesión alguno de los campos es incorrecto'
-          return (
-            <Login
+        }
+      </LoginMutation>
+    )
+  }
+
+  const registerMutation = () => {
+    return (
+      <RegisterMutation>
+        {
+          (register, { data, loading, error }) => {
+            const onSubmit = ({ email, password }) => {
+              const input = { email, password }
+              const variables = { input }
+              register({ variables })
+                .then(({ data }) => {
+                  const { signup } = data
+                  activateAuth(signup)
+                })
+            }
+            const errorMsg = error && 'El usuario ya existe o hay algún problema'
+            return <Register
               disabled={loading}
               onSubmit={onSubmit}
               error={errorMsg}
             />
-          )
-        }
-      }
-    </LoginMutation>
-  )
-}
-
-const registerMutation = ({ isAuth, activateAuth }) => {
-  return (
-    <RegisterMutation>
-      {
-        (register, { data, loading, error }) => {
-          const onSubmit = ({ email, password }) => {
-            const input = { email, password }
-            const variables = { input }
-            register({ variables })
-              .then(({ data }) => {
-                const { signup } = data
-                activateAuth(signup)
-              })
           }
-          const errorMsg = error && 'El usuario ya existe o hay algún problema'
-          return <Register
-            disabled={loading}
-            onSubmit={onSubmit}
-            error={errorMsg}
-          />
         }
-      }
-    </RegisterMutation>
+      </RegisterMutation>
+    )
+  }
+  return (
+    <>
+      {loginMutation()}
+    </>
   )
 }
