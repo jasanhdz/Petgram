bc8db5a3398565c2e8e05369ef10d4f21a59af45
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-24 19:05:35 -0600

    integrating react with graphql using react-apollo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'https://petgramserver-gp0xvypt8.now.sh/graphql'
+})
 
 // ReactDOM.render(que vamos a renderizar, donde lo vamos a renderizar)
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app')
 )
