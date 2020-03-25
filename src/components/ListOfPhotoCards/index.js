3c2dd50889c4db5f91404031f885e5807a36d9c3
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-24 19:22:04 -0600

    separating higher order components for the data fetch

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,27 +1,10 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
 
-const withPhotos = graphql(gql`
-  query getPhotos($categoryId: ID) {
-  photos(categoryId: $categoryId) {
-    id
-    categoryId
-    src
-    likes
-    userId
-    liked
-  }
-}
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
       {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
