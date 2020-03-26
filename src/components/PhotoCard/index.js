21f1bb666367c87f33e2f56432a03640ecf943bb
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-25 22:35:23 -0600

    Using PropTypes to validate the props

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,6 +4,7 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 import { ImgWrapper, Img, Article } from './styles'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../containers/ToggleLikeMutation'
+import PropTypes from 'prop-types'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -38,4 +39,22 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
       }
     </Article>
   )
+}
+
+
+PhotoCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  likes: function (props, propName, componentName) {
+    const propValue = props[propName]
+
+    if (propValue === undefined) {
+      return new Error(`${propName} Value must be defined`)
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} Value must be greater than 0`)
+    }
+  }
 }
