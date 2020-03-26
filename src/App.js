62be66d7c8406be52938a9d55d44e39b1c142b31
Jasan Hernandez <jasan_azael@hotmail.es>
2020-03-25 22:12:59 -0600

    React.lazy() and component Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Router, Redirect } from '@reach/router'
@@ -7,16 +7,18 @@ import { Context } from './Context'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/NavBar'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 import { NotFound } from './pages/NotFound'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -34,6 +36,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </>
+    </Suspense>
   )
 }
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { FavsWithQuery } from '../containers/GetFavorites'
 import { Layout } from '../components/Layout'
 
-export const Favs = () => (
+const Favs = () => (
   <Layout show title='Tus favoritos' subtitle='aquí puedes encontrar tus favoritos'>
     <FavsWithQuery />
   </Layout>
 
 )
+
+export default Favs
